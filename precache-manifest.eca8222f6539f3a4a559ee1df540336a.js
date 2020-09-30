c0cfed51403edece3914e1435eecc2bcff185410
Oliver Kersey <oliverjkersey@gmail.com>
2020-09-30 10:27:16 -0400

    Updates

---
+++ precache-manifest.eca8222f6539f3a4a559ee1df540336a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945980ce715ab3ad1fee445b1f47847a",
+    "url": "/whaleMap/index.html"
+  },
+  {
+    "revision": "0765246370fd1b3aeb91",
+    "url": "/whaleMap/static/css/2.f961c21d.chunk.css"
+  },
+  {
+    "revision": "58d604ea91e9bd119bd2",
+    "url": "/whaleMap/static/css/main.3c3df528.chunk.css"
+  },
+  {
+    "revision": "0765246370fd1b3aeb91",
+    "url": "/whaleMap/static/js/2.696f15ff.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/whaleMap/static/js/2.696f15ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d604ea91e9bd119bd2",
+    "url": "/whaleMap/static/js/main.a33b02e4.chunk.js"
+  },
+  {
+    "revision": "bbace21f621dc1f55043",
+    "url": "/whaleMap/static/js/runtime-main.7c5fa9f5.js"
+  },
+  {
+    "revision": "64142b6d6d15ffadd6b3cae6552d72b9",
+    "url": "/whaleMap/static/media/dolphin.64142b6d.svg"
+  },
+  {
+    "revision": "e55e600d90cdebc2f1f13f90c39c90f7",
+    "url": "/whaleMap/static/media/dolphinButton.e55e600d.jpg"
+  },
+  {
+    "revision": "0f7b3e68f751beb0393eb3e141ccf0b7",
+    "url": "/whaleMap/static/media/graywhale.0f7b3e68.svg"
+  },
+  {
+    "revision": "9738490492f646e5a6bab2108933e583",
+    "url": "/whaleMap/static/media/graywhaleButton.97384904.jpg"
+  },
+  {
+    "revision": "a95f0de32f52dca308c395aded6dd232",
+    "url": "/whaleMap/static/media/harborporpoise.a95f0de3.svg"
+  },
+  {
+    "revision": "d8fa608fbda94e6c21058da438149329",
+    "url": "/whaleMap/static/media/harborseal.d8fa608f.svg"
+  },
+  {
+    "revision": "5c021703c2a6a27588483d7ffee458a9",
+    "url": "/whaleMap/static/media/harborsealButton.5c021703.jpg"
+  },
+  {
+    "revision": "e1f67d62075ab5fd9f7a391142200102",
+    "url": "/whaleMap/static/media/humpback.e1f67d62.svg"
+  },
+  {
+    "revision": "93e89dca4944b29014c4ede0c23ff209",
+    "url": "/whaleMap/static/media/humpbackButton.93e89dca.jpg"
+  },
+  {
+    "revision": "d0494b781eb24e047b4550156841178c",
+    "url": "/whaleMap/static/media/multicolor-border.d0494b78.png"
+  },
+  {
+    "revision": "b86180c8f8ee1014cf74b95c555d7fdd",
+    "url": "/whaleMap/static/media/nauticalPaper.b86180c8.jpg"
+  },
+  {
+    "revision": "a1767322ae71834e27785c52831a8494",
+    "url": "/whaleMap/static/media/nesButton.a1767322.jpg"
+  },
+  {
+    "revision": "e589b51b8c5bdb7e6207dc1d590ca796",
+    "url": "/whaleMap/static/media/northernseal.e589b51b.svg"
+  },
+  {
+    "revision": "5d0aa9d68645e4628c636f79936fef39",
+    "url": "/whaleMap/static/media/ocean.5d0aa9d6.jpg"
+  },
+  {
+    "revision": "b5c2987bd54970abb30cc4c93bc22f22",
+    "url": "/whaleMap/static/media/orca.b5c2987b.svg"
+  },
+  {
+    "revision": "37851ba7abcd8d8479cac88f243874a7",
+    "url": "/whaleMap/static/media/pacificdolphin.37851ba7.svg"
+  },
+  {
+    "revision": "cb05e5c61bfcb883f769be8bd22ee5e3",
+    "url": "/whaleMap/static/media/pacificdolphinButton.cb05e5c6.jpg"
+  },
+  {
+    "revision": "e48c3def9c6c24e36772f3418c262852",
+    "url": "/whaleMap/static/media/ropeBorder.e48c3def.png"
+  },
+  {
+    "revision": "a1bb209feed8220fcf5627c8580456c9",
+    "url": "/whaleMap/static/media/scrollBackground.a1bb209f.jpg"
+  },
+  {
+    "revision": "5e33a58921a04465539a7624c6b30042",
+    "url": "/whaleMap/static/media/seaotter.5e33a589.svg"
+  },
+  {
+    "revision": "acad40a1b5b9777901b3dc2c0805c3bc",
+    "url": "/whaleMap/static/media/seaotterButton.acad40a1.jpg"
+  },
+  {
+    "revision": "d91067c61282ba0d60f4b32639ea4d57",
+    "url": "/whaleMap/static/media/ship.d91067c6.svg"
+  },
+  {
+    "revision": "88b246ead4a11c30c642a2e83532dde7",
+    "url": "/whaleMap/static/media/shipAnimationImage.88b246ea.png"
+  },
+  {
+    "revision": "374df623a770942c02e72b2a365a1d7c",
+    "url": "/whaleMap/static/media/wavesPattern.374df623.jpg"
+  },
+  {
+    "revision": "a1ce04da8cecbe8e56dc07efa8f0a852",
+    "url": "/whaleMap/static/media/whale.a1ce04da.svg"
+  },
+  {
+    "revision": "4400c796143dacfd4cf6bb5ef547c774",
+    "url": "/whaleMap/static/media/wheel.4400c796.svg"
+  },
+  {
+    "revision": "4234931aa824a9979a9a8afa5b59a8e2",
+    "url": "/whaleMap/static/media/woodBorder.4234931a.png"
+  }
+]);
